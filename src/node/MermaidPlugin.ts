851ac1cd9022316b31ae36df7b9bf799be497bba
himcs <787632628@qq.com>
2022-08-02 13:10:01 +0800

    1

--- src/node/MermaidPlugin.ts
+++ src/node/MermaidPlugin.ts
@@ -1,5 +1,33 @@
 import type {Plugin} from '@vuepress/core'
 import {hash, path} from '@vuepress/utils'
+const puppeteer = require('puppeteer')
+const path = require('path')
+const backgroundColor = 'white'
+getSVG = async (definition) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(`file://${path.join(__dirname, '../index.html')}`)
+    const result = await page.$eval('#container', (container, definition) => {
+      container.textContent = definition
+      try {
+        window.mermaid.initThrowsErrors(undefined, container)
+        return { status: 'success' }
+      } catch (error) {
+        return { status: 'error', error, message: error.message }
+      }
+    }, definition)
+    if (result.status === 'error') {
+      console.error(result.message)
+    }
+    const svg = await page.$eval('#container', (container,) => {
+      const svg = container.getElementsByTagName?.('svg')?.[0]
+      svg.style.backgroundColor = backgroundColor
+      return container.innerHTML
+    })
+    const svgXML = convertToValidXML(svg)
+    await browser.close()
+    return svgXML
+  }
 
 
 //注册组件
@@ -8,9 +36,7 @@ const mermaidChart = (tokens, idx, options, env, slf) => {
         const token = tokens[idx]
         const key = `mermaid_${hash(idx)}`
         const {content} = token
-
-        let encoded = encodeURIComponent(content)
-        return `<Mermaid id="${key}" graph="${encoded}"></Mermaid>`
+        return await getSVG(token)
     } catch ({str, hash}) {
         return `<pre>${str}</pre>`
     }
